0d0e550e80fc46433bb2321f9a39972c9d410cd9
Daniel Tang <tang.d@hotmail.com>
2022-05-25 23:51:44 +0800

    Minor formatting (black)

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,4 +2,4 @@
 import sys
 import os
 
-sys.path.append(os.path.join("/..","/src/"))
+sys.path.append(os.path.join("/..", "/src/"))
--- tests/test_preparation.py
+++ tests/test_preparation.py
@@ -1,27 +1,21 @@
-
 # Public modules
 import pytest
 
 # Internal modules
-from src.preparation import DataGetter 
+from src.preparation import DataGetter
 
 # Fixtures
+
 
 @pytest.fixture
 def myDataGetter():
     return DataGetter()
 
-@pytest.mark.parametrize(
-    "parameter",
-    [
-        "website",
-        "downloadLinksList",
-        "dataFolder"
-    ]
 
-)
-def test_DataGetter_has_attributes(myDataGetter,parameter):
-    assert getattr(myDataGetter,parameter) is not None
+@pytest.mark.parametrize("parameter", ["website", "downloadLinksList", "dataFolder"])
+def test_DataGetter_has_attributes(myDataGetter, parameter):
+    assert getattr(myDataGetter, parameter) is not None
+
 
 def test_DataGetter_runs(myDataGetter):
     myDataGetter.run()
